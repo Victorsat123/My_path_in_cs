96b1758d20da3a312eb2a09f494813800ca25d98
Victor <kwolen123@gmail.com>
2026-03-26 21:06:49 +0200

    1

--- OPP/database.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+pymysql://root:victor123321@localhost:3306/progress_hub"
-engine = create_engine(DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
