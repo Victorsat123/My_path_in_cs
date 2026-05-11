60b470357c49d36540768e45053d9462d605d0c1
Victor <kwolen123@gmail.com>
2026-05-11 13:20:10 +0300

    Додав нові файли лабораторок

--- Web_design/demo_13_1.js
+++ Web_design/demo_13_1.js
@@ -1,7 +1,7 @@
 let currentDate = new Date();
 
 let day = currentDate.getDate();
-let month = currentDate.getMonth() + 1; // Додаємо 1, бо місяці з 0
+let month = currentDate.getMonth() + 1; 
 let year = currentDate.getFullYear();
 
 if (day < 10) day = '0' + day;
